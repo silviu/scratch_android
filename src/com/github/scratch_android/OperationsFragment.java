8f7c121cb241a0cf96b13859d4caf61ffb701227
Silviu Grijincu <silviu.grijincu@gmail.com>
2012-11-17 00:31:00 +0200

    Recreated the Motion code block view.

--- src/com/github/scratch_android/OperationsFragment.java
+++ src/com/github/scratch_android/OperationsFragment.java
@@ -5,13 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class OperationsFragment extends Fragment {
-
+	RelativeLayout lay;
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.operations_layout, container, false);
+		View v = inflater.inflate(R.layout.motion_blocks, container, false);
+		return v;
+	}
+	
+	private void open_images() {
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 100);
+		params.leftMargin = 25;
+		params.topMargin = 100;
+		MotionCodeBlock op = new MotionCodeBlock(lay.getContext());
+		lay.addView(op, params);
 	}
 }
