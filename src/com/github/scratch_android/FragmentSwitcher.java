70da469b3ac30d177f0cf2ac733ee366bf939bfa
Silviu Grijincu <silviu.grijincu@gmail.com>
2012-11-18 22:38:07 +0200

    Added a on click manager

--- src/com/github/scratch_android/FragmentSwitcher.java
+++ src/com/github/scratch_android/FragmentSwitcher.java
@@ -1,5 +1,7 @@
 package com.github.scratch_android;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,17 +9,49 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class FragmentSwitcher extends Fragment implements View.OnClickListener{
+public class FragmentSwitcher extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_switcher_layout, container, false);
+		new Manager(v);
 		return v;
 	}
+}
 
-	@Override
-	public void onClick(View v) {
+class Manager implements View.OnClickListener {
+	private View v;
+	private ArrayList<ToggleFragmentButton> listeners = new ArrayList<ToggleFragmentButton>();
+
+	private final int N = 8;
+	private final int[] buttons = { R.id.motion_button_id,    R.id.looks_button_id, 
+									  R.id.sound_button_id,     R.id.pen_button_id, 
+									  R.id.control_button_id,   R.id.sensing_button_id, 
+									  R.id.operators_button_id, R.id.variables_button_id};
+	public Manager(View v) {
+		this.v = v;
+		construct_listeners();
 	}
-
+	
+	public void construct_listeners() {
+		for (int i = 0; i < N; i++) {
+			ToggleFragmentButton tfb = (ToggleFragmentButton) this.v.findViewById(buttons[i]);
+			tfb.setOnClickListener(this);
+			listeners.add(tfb);
+		}
+	}
+	@Override
+	public void onClick(View button) {
+		for (ToggleFragmentButton bt : listeners) {
+			if (bt == button) {
+				bt.toggle();
+				continue;
+			}
+			if (bt.is_toggled())
+				bt.untoggle();
+		}
+		
+	}
+	
 }
--- src/com/github/scratch_android/ToggleFragmentButton.java
+++ src/com/github/scratch_android/ToggleFragmentButton.java
@@ -1,40 +1,34 @@
 package com.github.scratch_android;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 
-public class ToggleFragmentButton extends ImageView implements OnToggleListener{
+public class ToggleFragmentButton extends ImageView{
 	private String selected_image_name;
 	private String unselected_image_name;
 	private int id;
 	private boolean is_selected = false;
 	
-	private ArrayList<ToggleFragmentButton> listeners = new ArrayList<ToggleFragmentButton>();
-	private final int N = 8;
-	private final int[] buttons = { R.id.motion_button_id, R.id.looks_button_id, 
-									  R.id.sound_button_id, R.id.pen_button_id, 
-									  R.id.control_button_id, R.id.sensing_button_id, 
-									  R.id.operations_fragment, R.id.variables_button_id};
 	public ToggleFragmentButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		setOnClickListener(this);
 		init(context, attrs);
-		construct_listeners();
 	}
 	
-	private void construct_listeners() {
-		for (int i = 0; i < N; i++) {
-			if (buttons[i] == this.id)
-				continue;
-			listeners.add((ToggleFragmentButton) findViewById(buttons[i]));
-		}
-		Log.v("??????????????", String.valueOf(listeners.size()));
+	public boolean is_toggled() {
+		return this.is_selected;
+	}
+	
+	public void untoggle() {
+		this.setImageResource(getResources().getIdentifier(this.unselected_image_name, "drawable", "com.github.scratch_android"));	
+		is_selected = false;
+	}
+	
+	public void toggle() {
+		this.setImageResource(getResources().getIdentifier(this.selected_image_name, "drawable", "com.github.scratch_android"));	
+		is_selected = true;
 	}
 	
 	private void init(Context context, AttributeSet attrs) {
@@ -57,33 +51,4 @@ public class ToggleFragmentButton extends ImageView implements OnToggleListener{
             	this.is_selected = true;
         a.recycle();
 	}
-
-	@Override
-	public void onClick(View view) {
-		
-		Log.v("Button", "Clicked");
-		if (is_selected) {
-			return;
-		}
-		else {
-			for (ToggleFragmentButton l : listeners)
-	            l.onToggle();
-	    }
-
-			this.setImageResource(getResources().getIdentifier(this.selected_image_name, "drawable", "com.github.scratch_android"));	
-			is_selected = true;
-		}
-
-	@Override
-	public void onToggle() {
-		if (is_selected) {
-			this.setImageResource(getResources().getIdentifier(this.unselected_image_name, "drawable", "com.github.scratch_android"));	
-			is_selected = false;
-		}
-	}
 }
-
-interface OnToggleListener extends View.OnClickListener{
-    public void onToggle();
-}
-
